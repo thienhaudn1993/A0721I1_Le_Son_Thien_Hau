d2a4e06d017fafc6c2162426f9e1ffb7f5e87d4a
thienhaudn1993 <thienhaudn@gmail.com>
2021-08-05 16:47:53 +0700

    bai tap js

--- module 1/bai7_bien_kieu_du_lieu_va_toan_tu/thuc_hanh/Su_dung_toan_tu/ThucHanhToanTu.js
+++ module 1/bai7_bien_kieu_du_lieu_va_toan_tu/thuc_hanh/Su_dung_toan_tu/ThucHanhToanTu.js
@@ -1,5 +1,10 @@
+let inputWidth;
+let inputHeight;
 inputWidth=prompt('Nhập chiều rộng');
 inputHeight=prompt('Nhập chiều cao');
+let width = parseInt(inputWidth);
+let height = parseInt(inputHeight);
+let area = width * height;
 document.write("dien tich HCN: " +area);
 
 

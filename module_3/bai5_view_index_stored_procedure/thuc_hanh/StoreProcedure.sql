f4993c2571668b605cfe99d60fffc198a6270389
unknown <you@example.com>
2021-12-10 12:44:02 +0700

    bt

---
+++ module_3/bai5_view_index_stored_procedure/thuc_hanh/StoreProcedure.sql
@@ -0,0 +1,22 @@
+use mysqlsampledatabase;
+-- Tạo Mysql Stored Procedure đầu tiên
+DELIMITER //
+CREATE PROCEDURE findAllCustomers()
+BEGIN
+  SELECT * FROM customers;
+END //
+DELIMITER ;
+call findAllCustomers();
+
+-- Sửa procedure
+-- Trong Mysql không cung cấp lệnh sửa Stored nên thông thường chúng ta sẽ chạy lệnh tạo mới.
+-- Lệnh Drop để xóa đi Procedure đó và tạo lại:
+DELIMITER //
+DROP PROCEDURE IF EXISTS `findAllCustomers`//
+CREATE PROCEDURE findAllCustomers()
+BEGIN
+SELECT * FROM customers where customerNumber = 175;
+END //
+
+
+

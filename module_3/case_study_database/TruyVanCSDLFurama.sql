c9f918f5f6a35cc3cdc534f537297caff7ac051d
unknown <you@example.com>
2021-12-13 18:26:29 +0700

    bt

--- module_3/case_study_database/TruyVanCSDLFurama.sql
+++ module_3/case_study_database/TruyVanCSDLFurama.sql
@@ -107,8 +107,19 @@ group by(hop_dong.ma_khach_hang) order by  so_lan_dat_phong asc;
 		-- tong_tien (Với tổng tiền được tính theo công thức như sau: Chi Phí Thuê + Số Lượng * Giá, với Số Lượng và Giá là từ bảng dich_vu_di_kem, hop_dong_chi_tiet) 
 		-- cho tất cả các khách hàng đã từng đặt phòng. (những khách hàng nào chưa từng đặt phòng cũng phải hiển thị ra).
         
-        
-        
+ select khach_hang.ma_khach_hang, khach_hang.ho_ten, loai_khach.ten_loai_khach, hop_dong.ma_hop_dong, dich_vu.ten_dich_vu, hop_dong.ngay_lam_hd, hop_dong.ngay_ket_thuc, 
+ sum(dich_vu.chi_phi_thue + hop_dong_chi_tiet.so_luong * dich_vu_di_kem.gia) as "tong_tien"
+ from khach_hang 
+ left join loai_khach on khach_hang.ma_loai_khach = loai_khach.ma_loai_khach
+ left join hop_dong on khach_hang.ma_khach_hang = hop_dong.ma_khach_hang
+ left join dich_vu on dich_vu.ma_dich_vu = hop_dong.ma_dich_vu
+ left join hop_dong_chi_tiet on hop_dong_chi_tiet.ma_hop_dong = hop_dong.ma_hop_dong
+ left join dich_vu_di_kem on dich_vu_di_kem.ma_dich_vu_di_kem = hop_dong_chi_tiet.ma_dich_vu_di_kem
+ group by(hop_dong.ma_hop_dong);
+ 
+ -- Câu 6.	Hiển thị ma_dich_vu, ten_dich_vu, dien_tich, chi_phi_thue, ten_loai_dich_vu 
+	 -- của tất cả các loại dịch vụ chưa từng được khách hàng thực hiện đặt từ quý 1 của năm 2021 (Quý 1 là tháng 1, 2, 3).
+ select dich_vu.ma_dich_vu, dich_vu.ten_dich_vu, dich_vu.dien_tich, dich_vu.chi_phi_thue, loai_dich_vu.ten_loai_dich_vu      
         
         
         

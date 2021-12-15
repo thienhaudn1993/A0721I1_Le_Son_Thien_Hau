219bdc533c7d80a4554026905a3cb639135898c3
thienhaudn1993 <thienhaudn@gmail.com>
2021-12-15 16:46:39 +0700

    bt

--- module_3/case_study_database/TruyVanCSDLFurama.sql
+++ module_3/case_study_database/TruyVanCSDLFurama.sql
@@ -160,9 +160,21 @@ group by thang order by thang ;
 
 select hop_dong.ma_hop_dong, hop_dong.ngay_lam_hd, hop_dong.ngay_ket_thuc, hop_dong.tien_dat_coc, sum(hop_dong_chi_tiet.so_luong) as "so_luong_dich_vu_di_kem"
 from hop_dong
-left join hop_dong_chi_tiet on hop_dong_chi_tiet.ma_hop_dong = hop_dong.ma_hop_dong group by hop_dong.ma_hop_dong
-;
-        
-        
-        
-        
+left join hop_dong_chi_tiet on hop_dong_chi_tiet.ma_hop_dong = hop_dong.ma_hop_dong group by hop_dong.ma_hop_dong;
+
+-- Câu 11.	Hiển thị thông tin các dịch vụ đi kèm đã được sử dụng bởi những khách hàng có 
+		 -- ten_loai_khach là “Diamond” và có dia_chi ở “Vinh” hoặc “Quảng Ngãi”.
+         
+select dich_vu_di_kem.ma_dich_vu_di_kem, dich_vu_di_kem.ten_dich_vu_di_kem
+from dich_vu_di_kem 
+inner join hop_dong_chi_tiet on hop_dong_chi_tiet.ma_dich_vu_di_kem = dich_vu_di_kem.ma_dich_vu_di_kem
+inner join hop_dong on hop_dong.ma_hop_dong = hop_dong_chi_tiet.ma_hop_dong
+inner join khach_hang on khach_hang.ma_khach_hang = hop_dong.ma_khach_hang
+inner join loai_khach on loai_khach.ma_loai_khach = khach_hang.ma_loai_khach where loai_khach.ten_loai_khach = "Diamond"
+and khach_hang.dia_chi like "%Vinh%" or khach_hang.dia_chi like "%Quảng Ngãi%";
+
+-- Câu 12.	Hiển thị thông tin ma_hop_dong, ho_ten (nhân viên), ho_ten (khách hàng), so_dien_thoai (khách hàng), 
+-- ten_dich_vu, so_luong_dich_vu_di_kem (được tính dựa trên việc sum so_luong ở dich_vu_di_kem), tien_dat_coc 
+-- của tất cả các dịch vụ đã từng được khách hàng đặt vào 3 tháng cuối năm 2020 nhưng chưa từng được khách hàng đặt vào 6 tháng đầu năm 2021.
+
+select hop_dong.ma_hop_dong, nhan_vien.ho_ten, khach_hang.ho_ten, khach_hang.so_dien_thoai, 

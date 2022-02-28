866102e09a848f6cbd4cc47e65ccaca637c81573
unknown <you@example.com>
2022-02-28 13:45:32 +0700

    bt

--- module_4/bai6_jap_springboot/demo_customer_managerment/src/main/java/com/codegym/customer_managerment/service/CustomerService.java
+++ module_4/bai6_jap_springboot/demo_customer_managerment/src/main/java/com/codegym/customer_managerment/service/CustomerService.java
@@ -6,7 +6,10 @@ import java.util.List;
 
 public interface CustomerService {
     List<Customer> findAll();
+
     void saveCustomer(Customer customer);
+
     void deleteCustomer(Long id);
+
     Customer findCustomerById(Long id);
 }
